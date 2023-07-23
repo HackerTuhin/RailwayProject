160674f94a3b42ee59381d4a4cbb3e4dab097a13
CoderTuhin <86509674+HackerTuhin@users.noreply.github.com>
2023-07-23 21:51:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ connection.connect((err) => {
         console.log(err)
     else {
         console.log("Connected to DB")
-        app.listen(5000,'localhost', () => {
+        app.listen(5000,() => {
             console.log('listening on port 3000');
         });
     }
